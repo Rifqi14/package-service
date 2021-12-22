de40155b18a8d77a7066ef992fecde298ddab402
rifqi.shoesmart <rifqi.shoesmart@gmail.com>
2021-12-22 11:46:27 +0700

    change url google login

--- google/oauth.go
+++ google/oauth.go
@@ -7,9 +7,9 @@ import (
 	"net/http"
 )
 
-const OauthGoogleUrlAPI = "https://people.googleapis.com/v1/people/me?personFields=names,emailAddresses,genders,birthdays&access_token="
+const OauthGoogleUrlAPI = "https://people.googleapis.com/v1/people/me?personFields=names%2CemailAddresses%2Cgenders%2Cbirthdays&access_token="
 
-func GetGoogleProfile(token string) (res map[string]interface{},err error) {
+func GetGoogleProfile(token string) (res map[string]interface{}, err error) {
 	response, err := http.Get(OauthGoogleUrlAPI + token)
 	if err != nil {
 		fmt.Println("error token")
@@ -17,25 +17,24 @@ func GetGoogleProfile(token string) (res map[string]interface{},err error) {
 	}
 
 	defer response.Body.Close()
-	if response.StatusCode != 200{
-		return res,err
+	if response.StatusCode != 200 {
+		return res, err
 	}
 	responseBody, err := ioutil.ReadAll(response.Body)
-
 	if err != nil {
 		fmt.Println("error read body")
 
 		return res, err
 	}
-	err = json.Unmarshal(responseBody,&res)
+	err = json.Unmarshal(responseBody, &res)
 	if err != nil {
-		return res,err
+		return res, err
 	}
 
-	return res,err
+	return res, err
 }
 
-func getUser(token string) (res []byte,err error) {
+func getUser(token string) (res []byte, err error) {
 	response, err := http.Get(OauthGoogleUrlAPI + token)
 	if err != nil {
 		fmt.Println("error token")
