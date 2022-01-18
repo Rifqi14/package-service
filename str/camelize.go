50c484663232940a951f6bbae51ef1a932c9aa52
rifqi.shoesmart <rifqi.shoesmart@gmail.com>
2022-01-18 14:17:51 +0700

    add slug package

--- str/camelize.go
+++ str/camelize.go
@@ -25,6 +25,12 @@ func (b *buffer) indent() {
 	}
 }
 
+func (b *buffer) dash() {
+	if len(b.r) > 0 {
+		b.r = append(b.r, '-')
+	}
+}
+
 // Underscore ...
 func Underscore(s string) string {
 	b := buffer{
@@ -55,6 +61,42 @@ func Underscore(s string) string {
 	if m != 0 {
 		if !w {
 			b.indent()
+		}
+		b.write(m)
+	}
+
+	return string(b.r)
+}
+
+func Slug(s string) string {
+	b := buffer{
+		r: make([]byte, 0, len(s)),
+	}
+	var m rune
+	var w bool
+	for _, ch := range s {
+		if unicode.IsUpper(ch) {
+			if m != 0 {
+				if !w {
+					b.dash()
+					w = true
+				}
+				b.write(m)
+			}
+			m = unicode.ToLower(ch)
+		} else {
+			if m != 0 {
+				b.dash()
+				b.write(m)
+				m = 0
+				w = false
+			}
+			b.write(ch)
+		}
+	}
+	if m != 0 {
+		if !w {
+			b.dash()
 		}
 		b.write(m)
 	}
